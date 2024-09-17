7a32ab80e2cfb2bb46799643c5dd38977ae31d78
Matt Sherrill <98839754+mdsherrill@users.noreply.github.com>
2024-09-17 13:54:39 -0500

    Removed unsafe perms

--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
-const { app, BrowserWindow } = require('electron');
-const fs = require('fs');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const fs = require("fs");
+const path = require("path");
 
 const createWindow = () => {
   const nonce = Math.random().toString(36).substr(2, 15); // Generate a random nonce
   const win = new BrowserWindow({
     width: 1800,
     height: 1600,
-    icon: __dirname + './img/rocket_icon.png', 
+    icon: __dirname + "./img/rocket_icon.png",
     webPreferences: {
-      webSecurity: false,
-      // nodeIntegration: false,
-      // contextIsolation: true,
+      nodeIntegration: false,
+      contextIsolation: true,
     },
   });
 
   // Load HTML file
-  win.loadFile(path.join(__dirname, 'main.html'))
+  win.loadFile(path.join(__dirname, "main.html"));
 };
 
 app.whenReady().then(createWindow);
