6fb6b6aa8a9d205a0579f7fcf70c66e262d7fb30
Matt Sherrill <98839754+mdsherrill@users.noreply.github.com>
2024-09-18 17:42:18 -0500

    Fix icon path

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -4,15 +4,16 @@ const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 1800,
-    height: 1600,
-    icon: __dirname + "./assets/img/rocket_icon.png", // PATH from src folder
+    width: 800,
+    height: 600,
+    icon: __dirname + './../../assets/img/rocket_icon.png', // PATH from src folder
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
 
+  console.log("ICON PATH: " + win.icon);
   win.loadFile(path.join(__dirname, "main.html"));
 };
 
